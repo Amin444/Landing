348b39760e23c842fcb621b08c24e552c8a98990
Amin Mirzoev <aminmirzoev123@gmail.com>
2021-07-31 21:37:24 +0500

    li

---
+++ src/Page/Chikato.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import '../style/Chikato.css'
+
+ const Chikato = () => {
+  return(
+    <div className='root'> 
+     <div className='img-Blocks1'></div>
+     <div className='img-Blocks2'></div>
+     <div className='img-Blocks3'>
+      <div className='text'>
+      <h1 className='rfr'>DELIEVERY</h1>
+      <p className='p'>
+       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas praesentium earum esse fuga officia blanditiis ab laudantium, dolorum ratione explicabo recusandae nobis id voluptate excepturi eius porro expedita possimus nemo!
+      </p>
+      </div>
+     </div>
+     <div className='img-Blocks4'>
+     <div className='text2'>
+      <h1 className='rfr2'>DUBAI MALL</h1>
+      <p className='p2'>
+       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas praesentium earum esse fuga officia blanditiis ab laudantium, dolorum ratione explicabo recusandae nobis id voluptate excepturi eius porro expedita possimus nemo!
+      </p>
+      </div>
+     </div>
+     <div className='img-Blocks5'>
+     </div>
+    
+      <div className='img-Blocks6'>
+      <div className='About'>
+        <div>
+        <h1 className='rfr3'>ABOUT US</h1>
+        <p className='p3'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi repellendus amet neque vel sunt non tempore eveniet, praesentium atque 
+        et dicta saepe commodi quisquam mollitia dolor reiciendis. Aliquid natus eaque suscipit ad sint nisi
+         laborum quidem aperiam necessitatibus deserunt distinctio deleniti delectus error aspernatur dolor blanditiis assumenda, ipsa maiores animi.</p>
+        </div>
+     
+
+      <div className='contact'>
+        <p className='temp'>chikato@gmail.com 900 000 000  Dubai Mall</p> 
+        <p className='temp2'>Chikato coffee shop</p>
+       </div>
+     </div>
+    
+      </div>
+    </div>
+  )
+}
+
+export default Chikato
